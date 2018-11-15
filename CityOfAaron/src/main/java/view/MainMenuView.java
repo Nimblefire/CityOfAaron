374be639a3072415a800ee765b6f576006d6f06c
Andrea <Andrea@localhost>
2018-11-14 19:03:59 -0700

    Merge origin/master
    Conflicts:
    	CityOfAaron/src/main/java/view/NewGameView.java

--- CityOfAaron/src/main/java/view/MainMenuView.java
+++ CityOfAaron/src/main/java/view/MainMenuView.java
@@ -20,7 +20,7 @@ public class MainMenuView extends ViewBase {
                 + "N - Start a New Game\n"
                 + "L - Load a Saved Game\n"
                 + "H - Help Menu\n"
-                + "Q - Quit\n";
+                + "Q - Quit the Game\n";
     }
 
     /**
@@ -62,7 +62,7 @@ public class MainMenuView extends ViewBase {
                 helpMenu();
                 break;
             case "Q":
-                System.out.println("Hope to see you soon. Good-bye.");
+                System.out.println("\nHope to see you soon. Good-bye.");
                 // return false if you want this view to exit and return to the view that called it.
                 return false;
             default: 
@@ -82,8 +82,8 @@ public class MainMenuView extends ViewBase {
     }
 
     private void helpMenu(){
-        HelpMenuView view = new HelpMenuView();
-        view.displayView();
+        View helpMenu = new HelpMenuView();
+        helpMenu.displayView();
     }
 
     private void loadSavedGame(){

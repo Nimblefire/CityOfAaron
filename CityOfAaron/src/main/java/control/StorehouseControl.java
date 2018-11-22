8eac4ed30024d6cc3da20a48e6f46e50f3f82e97
Bellabot <bellabot@github.com>
2018-11-21 17:54:46 -0700

    Storehouse sortAnimal ages

--- CityOfAaron/src/main/java/control/StorehouseControl.java
+++ CityOfAaron/src/main/java/control/StorehouseControl.java
@@ -16,4 +16,20 @@ public class StorehouseControl {
         String name = null;
         return name;
     }
+    
+    
+    public static int[] sortAnimalAges(int[] age) {
+        
+        for (int i = 0; i < age.length -1; i++) {
+            int index = i;
+            for (int j = i + 1; j < age.length; j++) 
+                if (age[j] < age[index])
+                    index = j;
+            
+            int smallerNumber = age[index];
+            age[index] = age[i];
+            age[i] = smallerNumber;
+        }
+        return age;
+    }
 }
